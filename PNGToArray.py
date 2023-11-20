d60a9621d9dd035ca959b8c76bc892cfc3325ec2
stickstetris <stickstetris@gmail.com>
2023-11-19 17:21:41 -1000

    Added tiles for broken pipe seams

--- PNGToArray.py
+++ PNGToArray.py
@@ -27,7 +27,7 @@ def compare_tiles(tile, reference_tiles):
     best_match_name = ""
     best_match_score = float('-inf')  # Initialize with a low value for correlation
 
-    for tile_name, reference_tile in zip(['bridge', 'empty', 'forest', 'mountain', 'ocean', 'ocean2', 'pipe', 'pipe2', 'plain', 'river', 'road', 'road2', 'shoal'], reference_tiles):
+    for tile_name, reference_tile in zip(['bridge', 'brokenpipe', 'brokenpipe2', 'empty', 'forest', 'mountain', 'ocean', 'ocean2', 'pipe', 'pipe2', 'plain', 'river', 'road', 'road2', 'shoal'], reference_tiles):
         hist_comparison = cv2.compareHist(cv2.calcHist([tile], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256]),
                                           cv2.calcHist([reference_tile], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256]),
                                           cv2.HISTCMP_CORREL)
@@ -59,6 +59,8 @@ def getMapArray():
 
     # Load reference tiles
     bridge = cv2.imread('AWBW-Map-Reader/tiles/bridge.png')
+    brokenpipe = cv2.imread('AWBW-Map-Reader/tiles/brokenpipe.png')
+    brokenpipe2 = cv2.imread('AWBW-Map-Reader/tiles/brokenpipe2.png')
     empty = cv2.imread('AWBW-Map-Reader/tiles/empty.png')
     forest = cv2.imread('AWBW-Map-Reader/tiles/forest.png')
     mountain = cv2.imread('AWBW-Map-Reader/tiles/mountain.png')
@@ -73,7 +75,7 @@ def getMapArray():
     shoal = cv2.imread('AWBW-Map-Reader/tiles/shoal.png')
     shoal2 = cv2.imread('AWBW-Map-Reader/tiles/shoal.png')
 
-    reference_tiles = [bridge, empty, forest, mountain, ocean, ocean2, pipe, pipe2, plain, river, road, road2, shoal, shoal2]
+    reference_tiles = [bridge, brokenpipe, brokenpipe2, empty, forest, mountain, ocean, ocean2, pipe, pipe2, plain, river, road, road2, shoal, shoal2]
 
     # Set the tile size
     tile_size = 16
