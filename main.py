e7ddd1ceeb2a86f1b2358c9e0670dc8296bcdd7b
Sticks <stickstetris@gmail.com>
2023-11-20 15:20:47 -1000

    Rewrote the imports of main for better readability. Added a new tile, ocean3. main now allows user to use previous url, or input a new one.

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-import PNGToArray
-import getPNGAndHTML
-import animatedToArray
+from PNGToArray import getMapArray
+from getPNGAndHTML import downloadHTMLAndMapImage
+from animatedToArray import getGIFName
 import os
 
 pathToMap = 'AWBW-Map-Reader\saved\map.png'
-#getPNGAndHTML.getBoth('https://awbw.amarriner.com/prevmaps.php?maps_id=153739')
 
 ##If the files don't exist, download them through url
 if not os.path.exists(pathToMap):
-    url = input("Input the url to the map preview")
-    getPNGAndHTML.getBoth(url)
+    url = input("Input the url to the map preview: ")
+    downloadHTMLAndMapImage(url)
+else:
+    url = input("Input the url to the map preview, or leave blank to use the previous url: ")
+    if url != "":
+        downloadHTMLAndMapImage(url)
 
-#Create the map array with PNGToArray
-mapArray = PNGToArray.getMapArray()
+#Create the map array
+mapArray = getMapArray()
 
-#mapArray still has 'empty' tiles. Use animatedToArray to fill in the rest.
+#mapArray still has 'empty' tiles. Use getGIFName to fill in the rest.
 for i in range(len(mapArray)):
     for j in range(len(mapArray[i])):
         if mapArray[i][j] == 'empty':
-            tile = animatedToArray.findTile(j, i)
+            tile = getGIFName(j, i)
             if tile is not None:
                 mapArray[i][j] = tile
 
